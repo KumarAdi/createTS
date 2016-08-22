443375b8575574c097b95cd51e3b102f8b2d9ce6
Aditya Kumar <aditya.kumar.501203@gmail.com>
2016-08-22 15:26:59 -0700

    Fixed resolution scaling
    In the previous commit, the resolution didn't scale. Now it does. I also updated the docs, since I forgot to do that lasst time.

--- ts/Engine/Game.ts
+++ ts/Engine/Game.ts
@@ -9,13 +9,16 @@ class Game{
     /** The game's resolution */
     public resolution:Vector;
 
-    constructor(canvas:HTMLCanvasElement, width, height){
+    constructor(canvas:HTMLCanvasElement){
+        //set up game canvas
         this.canvas = canvas;
         this.stage = new createjs.Stage(canvas);
+        this.resolution = new Vector(canvas.width, canvas.height);
         this.resizeCanvas();
         window.addEventListener("resize", this.resizeCanvas);
 
-        let ball = new GameObject(100, 100, new createjs.SpriteSheet({
+        //test sprite, remove once usre cod ehsa been integrated
+        let ball = new GameObject(this.stage, 100, 100, new createjs.SpriteSheet({
             images: ["http://www.gritengine.com/luaimg/circle.png"],
             frames: {
                 width: 64,
@@ -27,23 +30,23 @@ class Game{
         this.stage.update();
     }
 
-    /** Resizes the canvas tot the screen*/
+    /** Resizes the canvas to the screen*/
     private resizeCanvas = () => {
         let windowRes = new Vector(window.innerWidth, window.innerHeight);
-        this.resolution = new Vector(this.canvas.width, this.canvas.height);
+        //figure out which dimension to fit to
         let scale = Math.min(
-            windowRes.x/this.resolution.x,
-            windowRes.y/this.resolution.y
+            windowRes.x/this.canvas.width,
+            windowRes.y/this.canvas.height
         );
-        console.log(scale);
-        this.stage.scaleX = scale;
-        this.stage.scaleY = scale;
+
+        //fit canvas to window
         this.canvas.width *= scale;
         this.canvas.height *= scale;
-        console.log({
-            w: this.canvas.width,
-            h: this.canvas.height
-        });
+        //Fit stage to canvas
+        this.stage.scaleX = this.canvas.width/this.resolution.x;
+        this.stage.scaleY = this.canvas.height/this.resolution.y;
+
+        //update the stage
         this.stage.update();
     }
 }
